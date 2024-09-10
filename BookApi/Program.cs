0b8d5981258bb4db6976af0e2517aae9e19dbf9c
Ives Chiba <ivesjundi@gmail.com>
2024-09-09 23:23:12 -0300

    chore :wrench: db conenction

--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -1,3 +1,6 @@
+using BookApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Get connection string from AppDbContext and applying before run the application
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
