df5b866492503e0d9596357272a14b8a069facbb
Ives Chiba <ivesjundi@gmail.com>
2024-09-10 09:08:34 -0300

    feat :sparkles: response model

---
+++ BookApi/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BookApi.Models;
+
+public class ResponseModel<T>
+{
+    public T? Dados { get; set; }
+    public string Menssagem { get; set; } = string.Empty;
+    public bool Status { get; set; } = true;
+}
